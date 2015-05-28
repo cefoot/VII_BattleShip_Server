f6aadd434d19d1de867e57ec215891e98598b09d
Chris Papenfuß <cefoot@outlook.de>
2015-05-28 19:40:32 +0200

    First Tests

---
+++ ShipTester/RunningGameTest.cs
@@ -0,0 +1,28 @@
+﻿using DE.Cefoot.BattleShips.Data;
+using DE.Cefoot.BattleShips.Server;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DE.Cefoot.BattleShips.Test
+{
+    [TestFixture]
+    public class RunningGameTest
+    {
+        [Test]
+        public void TestPlayer_Finished_Event()
+        {
+            var ply = new Player
+            {
+                Name = "Dummy"
+            };
+            var eventThrown = false;
+            RunningGame.PlayerFinishedEvent += (send, args) => eventThrown = true;
+            RunningGame.GetInfo(ply).GameFinished = true;
+            Assert.That(eventThrown);
+        }
+    }
+}
---
+++ ShipTester/ServerTest.cs
@@ -0,0 +1,32 @@
+﻿using DE.Cefoot.BattleShips.Server;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DE.Cefoot.BattleShips.Test
+{
+    [TestFixture]
+    public class ServerTest
+    {
+        [Test]
+        public void StartStop()
+        {
+            var serverExe = Assembly.GetAssembly(typeof(RunningGame)).CodeBase;
+            var server = Process.Start(serverExe);
+            Thread.Sleep(500);
+            Assert.That(server.Threads.Count, Is.EqualTo(13));
+            SendKeys.SendWait("x");
+            //Thread.Sleep(500);
+            server.WaitForExit(500);
+            Assert.That(server.HasExited, Is.True);
+
+        }
+    }
+}
